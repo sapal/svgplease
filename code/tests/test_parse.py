79ca64a012afa9ad8c44b023d039881bf4a18452
Michał Sapalski <sapalskimichal@gmail.com>
2014-03-13 13:59:43 +0100

    test_parse.py refactoring.

--- code/tests/test_parse.py
+++ code/tests/test_parse.py
@@ -3,23 +3,33 @@ import unittest
 import svgplease
 import svgplease.parse
 
-class ParseFilename(unittest.TestCase):
+class TestParse(unittest.TestCase):
+    """Base class for Parse* tests."""
+    tested_class_name = None
     def tokens(self, tokens):
         return svgplease.parse.join_tokens(tokens)
 
-    def parse(self, class_name, *tokens):
-        return getattr(svgplease.parse, class_name).parser().parse_text(self.tokens(tokens), eof=True)
+    def parse(self, *tokens):
+        return getattr(svgplease.parse, self.tested_class_name).parser().parse_text(
+                self.tokens(tokens), eof=True)
+
+    def assertParsingFailed(self, *tokens):
+        self.assertRaises(svgplease.parse.ParseError, self.parse, *tokens)
+
+class ParseFilename(TestParse):
+    tested_class_name = "Filename"
 
     def test_parse_normal_filename(self):
-        self.assertEqual(self.parse("Filename", "file.svg").filename, "file.svg")
+        self.assertEqual(self.parse("file.svg").filename, "file.svg")
 
     def test_parse_filename_with_whitespace(self):
-        self.assertEqual(self.parse("Filename", "file name.svg").filename, "file name.svg")
+        self.assertEqual(self.parse("file name.svg").filename, "file name.svg")
 
     def test_parse_special_filename(self):
-        self.assertRaises(svgplease.parse.ParseError, self.parse, "Filename", "then")
-        self.assertRaises(svgplease.parse.ParseError, self.parse, "Filename", "file")
+        self.assertParsingFailed("then")
+        self.assertParsingFailed("file")
 
     def test_parse_prefixed_filename(self):
-        self.assertEqual(self.parse("Filename", "file", "file.svg").filename, "file.svg")
+        self.assertEqual(self.parse("file", "file.svg").filename, "file.svg")
+
 
