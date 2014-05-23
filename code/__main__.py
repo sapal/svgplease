848c7c802da99a7abe345e7828196f356e3a2a21
Michał Sapalski <sapalskimichal@gmail.com>
2014-05-23 15:32:26 +0200

    .egg file support.

---
+++ code/__main__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+import sys
+from svgplease.main import run
+run(sys.argv[0], sys.argv[1:])
--- code/setup.py
+++ code/setup.py
@@ -9,6 +9,10 @@ setup(
     license="GPLv3",
     author="Michał Sapalski",
     author_email="sapalskimichal@gmail.com",
+    data_files=[
+        ("", ["__main__.py"]),
+        ("doc", ["../doc/grammar.txt", "../doc/change_detection_algorithm.txt"])
+    ],
     description="Command line tool for manipulating svg files.",
     #TODO: long_description=
     zip_safe=True,
@@ -16,7 +20,7 @@ setup(
         #TODO: add classifiers.
     ],
     platforms="any",
-    packages=["svgplease"],
+    packages=["svgplease", "modgrammar"],
     test_suite="tests.all_tests",
     #TODO: investigate if this should be True if some .svg files should be included in the package:
     include_package_data=False,
